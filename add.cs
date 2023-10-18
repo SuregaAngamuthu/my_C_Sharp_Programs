c8b799c5fa6aaa445af5fb62dc8f559ee4040834
Surega <surega.angamuthu2003@vakilsearch.com>
2023-10-18 12:25:49 +0530

    Initial commit off my Program

---
+++ add.cs
@@ -0,0 +1,14 @@
+using System;
+namespace hello
+{
+    class Hello
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
