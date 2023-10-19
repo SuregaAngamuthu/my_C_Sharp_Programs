8dba3a8318e3c0946f67455eb8b03f4e7a7ecab2
Surega <surega.angamuthu@vakilsearch.com>
2023-10-19 16:00:01 +0530

    first commit for this program

---
+++ enum.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int rollno = Convert.ToInt32(Name.daddy);
+            Console.WriteLine(rollno);
+            Console.ReadKey();
+        }
+        enum Name
+        {
+            surega=77,
+            uma,
+            daddy
+        };
+
+        
+    }
+}
